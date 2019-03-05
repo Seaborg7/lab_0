b13f249aba594ba3d440b9dca26a9969c552da71
202120 <202120@169.254.16.200>
2019-03-05 18:43:59 +0100

    Zaimplementowano funkcjonalość

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,15 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        List<String> lista = new ArrayList<String>();
+        if (value != null) {
+            for (int i = 0; i < value.length() + 1; i++) {
+                lista.add(value.substring(i, value.length()));
+            }
+
+        }
+
+        return lista;
     }
 
 }
